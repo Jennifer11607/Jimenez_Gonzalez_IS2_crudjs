ed5f3dab34240e19a0cb15b1aee583b3e51e4117
Jennifer11607 <jenniferjimenez152000@gmail.com>
2024-07-23 00:19:49 -0600

    cambios de crud js

---
+++ src/js/clientes/index.js
@@ -0,0 +1,181 @@
+const btnGuardar = document.getElementById('btnGuardar')
+const btnModificar = document.getElementById('btnModificar')
+const btnBuscar = document.getElementById('btnBuscar')
+const btnCancelar = document.getElementById('btnCancelar')
+const btnLimpiar = document.getElementById('btnLimpiar')
+const tablaClientes = document.getElementById('tablaClientes')
+const formulario = document.querySelector('form')
+
+btnModificar.parentElement.style.display = 'none'
+btnCancelar.parentElement.style.display = 'none'
+
+const getClientes = async () => {
+    const nombre = formulario.cli_nombre.value
+    const apellido = formulario.cli_apellido.value
+    const nit = formulario.cli_nit.value
+    const telefono = formulario.cli_telefono.value
+    const url = `/Jimenez_Gonzalez_IS2_crudjs/controllers/clientes/index.php?cli_nombre=${nombre}&cli_apellido=${apellido}&cli_nit=${nit}&cli_telefono=${telefono}`
+    const config = {
+        method: 'GET'
+    }
+
+    try {
+        const respuesta = await fetch(url, config);
+        const data = await respuesta.json();
+
+        tablaClientes.tBodies[0].innerHTML = ''
+        const fragment = document.createDocumentFragment()
+        let contador = 1;
+        console.log(data);
+        if (respuesta.status == 200) {
+            Swal.mixin({
+                toast: true,
+                position: "top-end",
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                icon: "success",
+                title: 'Clientes encontrados',
+                didOpen: (toast) => {
+                    toast.onmouseenter = Swal.stopTimer;
+                    toast.onmouseleave = Swal.resumeTimer;
+                }
+            }).fire();
+
+            if (data.length > 0) {
+                data.forEach(cliente => {
+                    const tr = document.createElement('tr')
+                    const celda1 = document.createElement('td')
+                    const celda2 = document.createElement('td')
+                    const celda3 = document.createElement('td')
+                    const celda4 = document.createElement('td')
+                    const celda5 = document.createElement('td')
+                    const celda6 = document.createElement('td')
+                    const celda7 = document.createElement('td')
+                    const buttonModificar = document.createElement('button')
+                    const buttonEliminar = document.createElement('button')
+
+                    celda1.innerText = contador;
+                    celda2.innerText = cliente.cli_nombre;
+                    celda3.innerText = cliente.cli_apellido;
+                    celda4.innerText = cliente.cli_nit;
+                    celda5.innerText = cliente.cli_telefono;
+
+
+                    buttonModificar.textContent = 'Modificar'
+                    buttonModificar.classList.add('btn', 'btn-warning', 'w-100')
+
+                    buttonEliminar.textContent = 'Eliminar'
+                    buttonEliminar.classList.add('btn', 'btn-danger', 'w-100')
+
+                    celda6.appendChild(buttonModificar)
+                    celda7.appendChild(buttonEliminar)
+
+                    tr.appendChild(celda1)
+                    tr.appendChild(celda2)
+                    tr.appendChild(celda3)
+                    tr.appendChild(celda4)
+                    tr.appendChild(celda5)
+                    tr.appendChild(celda6)
+                    tr.appendChild(celda7)
+                    fragment.appendChild(tr);
+
+                    contador++
+                });
+
+            } else {
+                const tr = document.createElement('tr')
+                const td = document.createElement('td')
+                td.innerText = 'No hay clientes'
+                td.colSpan = 7;
+
+                tr.appendChild(td)
+                fragment.appendChild(tr)
+            }
+        } else {
+            console.log('error al cargar clientes');
+        }
+
+        tablaClientes.tBodies[0].appendChild(fragment)
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getClientes();
+
+
+const guardarCliente = async (e) => {
+    e.preventDefault();
+    btnGuardar.disabled = true;
+
+    const url = '/Jimenez_Gonzalez_IS2_crudjs/controllers/clientes/index.php';
+    const formData = new FormData(formulario);
+    formData.append('tipo', 1);
+    formData.delete('cli_id');
+    const config = {
+        method: 'POST',
+        body: formData
+    };
+
+    try {
+        console.log('Enviando datos:', ...formData.entries());
+        const respuesta = await fetch(url, config);
+        const data = await respuesta.json();
+        console.log('Respuesta recibida:', data);
+        const { mensaje, codigo, detalle } = data;
+        if (respuesta.ok && codigo === 1) {
+            Swal.mixin({
+                toast: true,
+                position: "top-end",
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                icon: "success",
+                title: mensaje,
+                didOpen: (toast) => {
+                    toast.onmouseenter = Swal.stopTimer;
+                    toast.onmouseleave = Swal.resumeTimer;
+                }
+            }).fire();
+            getClientes();
+            formulario.reset();
+        } else {
+            console.log('Error:', detalle);
+            Swal.mixin({
+                toast: true,
+                position: "top-end",
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                icon: "error",
+                title: 'Error al guardar',
+                didOpen: (toast) => {
+                    toast.onmouseenter = Swal.stopTimer;
+                    toast.onmouseleave = Swal.resumeTimer;
+                }
+            }).fire();
+        }
+    } catch (error) {
+        console.log('Error de conexión:', error);
+        Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            icon: "error",
+            title: 'Error de conexión',
+            didOpen: (toast) => {
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
+            }
+        }).fire();
+    }
+    btnGuardar.disabled = false;
+};
+
+
+
+formulario.addEventListener('submit', guardarCliente)
+btnBuscar.addEventListener('click', getClientes)
